cb9287772533568bd95a8ffa8d10e7e21b2ac389
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-05-31 12:14:39 +0200

    add initImages() and add images

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,5 +1,13 @@
+// var's
 let images = []
 
+// main
+initImages()
+images.forEach(img => {
+    const main = document.getElementById('searchField')
+});
+
+// function's
 function getRandomInt(max, min = 0) {
     const d = max - min
     const r = Math.floor(d * Math.random())
@@ -7,5 +15,17 @@ function getRandomInt(max, min = 0) {
     console.log(n)
     return n
 }
+function initImages() {
+    images.push(initImage('../img/obj/bubble-navy-blue-th.png', 'navyBlue'))
+    images.push(initImage('../img/obj/bubble-red-th.png', 'red'))
+    images.push(initImage('../img/obj/bubble-sky-blue-th.png', 'skyBlue'))
+}
+function initImage(src, name) {
+    let i = {
+        name : name,
+        src : src
+    }
+    return i
+}
 
-// --- test
+// test
