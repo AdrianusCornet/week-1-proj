56e97dd0ac21c83535a9364a08b9131936abeda9
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-05-31 11:48:23 +0200

    add getRandomInt()

---
+++ resources/js/game.js
@@ -0,0 +1,11 @@
+let images = []
+
+function getRandomInt(max, min = 0) {
+    const d = max - min
+    const r = Math.floor(d * Math.random())
+    const n = min + r;
+    console.log(n)
+    return n
+}
+
+// --- test
